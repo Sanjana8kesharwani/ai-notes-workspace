2c13f61b8f138e99b907bd72dc5b0f246b860dce
Sanjana Kesharwani <sanjanakesharwani32@gmail.com>
2026-05-15 12:06:42 +0530

    Setup backend and user authentication structure

---
+++ backend/controllers/authController.js
@@ -0,0 +1,49 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const signupUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check all fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    // check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  signupUser,
+};
